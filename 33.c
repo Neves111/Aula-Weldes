e517d66a4716f514eb405177985031108d8511b6
Neves111 <otaviogneves11@gmail.com>
2024-09-13 01:06:26 -0300

    Create 33.c

---
+++ 33.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+
+    int a;
+
+    scanf("%d",&a);
+
+    for(int i = 0; i <= 10; i++) {
+        printf("%d\n", a*i);
+    }
+
+    return 0;
+}
