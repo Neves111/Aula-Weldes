525269474937000f807267efed34ba1e0aa701bd
Neves111 <otaviogneves11@gmail.com>
2024-09-12 23:35:45 -0300

    Create 21.c

---
+++ 21.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+   int a;
+
+   scanf("%d", &a);
+
+   if(a % 2 == 0) {
+        printf("par");
+   }else{
+        printf("impar");
+   }
+
+   return 0;
+}
