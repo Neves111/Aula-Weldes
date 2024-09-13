467942bd5162cd003e881029935f6df2e14d6d43
Neves111 <otaviogneves11@gmail.com>
2024-09-12 23:36:51 -0300

    Update 22.c

--- 22.c
+++ 22.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(void) {
+   int a;
+
+   scanf("%d", &a);
+
+   if(a % 4 == 0) {
+        printf("bissexto");
+   }else{
+        printf("nao bissexto");
+   }
+
+   return 0;
+}
