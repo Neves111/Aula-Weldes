c9f5362de337a42071b01edf9aaa8bca68e4112c
Neves111 <otaviogneves11@gmail.com>
2024-09-13 01:24:38 -0300

    Create 36.c

---
+++ 36.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+
+    int a;
+    int i = 0;
+    scanf("%d",&a);
+
+    while(i <= 10) {
+        printf("%d\n",a*i);
+        i++;
+    }
+    return 0;
+}
